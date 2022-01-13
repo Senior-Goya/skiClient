0973d1490fb2fda3ed0e32562c263d84ae77c2dd
Jorge Rivas <59454582+Senior-Goya@users.noreply.github.com>
2022-01-12 19:22:39 -0500

    added bread crumbs

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { ShopService } from './../shop.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService:ShopService, private activeRoute: ActivatedRoute) { }
+  constructor(private shopService:ShopService, private activeRoute: ActivatedRoute, private breadService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -20,6 +21,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     this.shopService.getProduct(+this.activeRoute.snapshot.paramMap.get('id')).subscribe(product =>  {
       this.product = product;
+      this.breadService.set('@productDetails', product.name);
     }, error => {
       console.log(error)
     });
